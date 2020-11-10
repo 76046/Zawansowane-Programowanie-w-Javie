49c212c2527ce7b2ae2b9329df1a0929a2e7214a
kornelusia141 <46178337+kornelusia141@users.noreply.github.com>
2020-11-10 19:57:55 +0100

    Java Swing Test

---
+++ SudokuProjekt/src/sudokuprojekt/SudokuProjekt.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sudokuprojekt;
+
+/**
+ *
+ * @author user
+ */
+public class SudokuProjekt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        OknoLogowania frame=new OknoLogowania();
+        frame.setVisible(true);
+    }
+    
+}
